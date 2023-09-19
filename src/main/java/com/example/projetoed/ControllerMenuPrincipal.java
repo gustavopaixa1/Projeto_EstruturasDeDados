0531b979fdb18069a547cd69c4ad777182e75b20
Lucas Rondineli <114787054+lucas-rondineli@users.noreply.github.com>
2023-09-19 19:28:32 -0300

    Correção de nomes.

--- src/main/java/com/example/projetoed/ControllerMenuPrincipal.java
+++ src/main/java/com/example/projetoed/ControllerMenuPrincipal.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+public class ControllerMenuPrincipal implements Initializable {
 
     @FXML
     private Button botaoDeEscolha;
--- src/main/java/com/example/projetoed/MainApplication.java
+++ src/main/java/com/example/projetoed/MainApplication.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("menuPrincipal.fxml"));

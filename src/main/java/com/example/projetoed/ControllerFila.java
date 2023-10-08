02b731f42220be1d10f4d834ea6fe2382f728a1f
gustavopaixa1 <luizgustavopaixa1@gmail.com>
2023-10-08 11:46:41 -0300

    Criando os arquivos FXML e as controladoras de cada arquivo. Criando os botões na tela inicial e ajustando o tamanho.

---
+++ src/main/java/com/example/projetoed/ControllerFila.java
@@ -0,0 +1,73 @@
+package com.example.projetoed;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+
+public class ControllerFila {
+
+    @FXML
+    private Button BotaoVoltar;
+
+    @FXML
+    private FlowPane FPDados;
+
+    @FXML
+    private TextField TFConsultaIndicePosicao;
+
+    @FXML
+    private TextField TFConsultaValorConteudo;
+
+    @FXML
+    private TextField TFInserirConteudo;
+
+    @FXML
+    private TextField TFInserirPosicao;
+
+    @FXML
+    private TextField TFNumeroDeElementos;
+
+    @FXML
+    private TextField TFRemoverPosicao;
+
+    @FXML
+    private AnchorPane paneConsultar1;
+
+    @FXML
+    private AnchorPane paneConsultar2;
+
+    @FXML
+    private AnchorPane paneInserir;
+
+    @FXML
+    private AnchorPane paneRemover;
+
+    @FXML
+    void EventoConsultaIndice(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EventoConsultaValor(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EventoInserir(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EventoRemover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EventoVoltar(MouseEvent event) {
+
+    }
+
+}

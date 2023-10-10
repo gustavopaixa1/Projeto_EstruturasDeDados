ea69abf8f8dc5f3bccb690bedecfcd13f3b8354e
Luiz Henrique <102159670+LuizHengs@users.noreply.github.com>
2023-10-09 22:33:31 -0300

    Modo inserir posto. Falta outras 3 funções

---
+++ src/main/java/com/example/projetoed/ControllerPilha.java
@@ -0,0 +1,195 @@
+package com.example.projetoed;
+
+import com.example.projetoed.implementations.Linked_Stack;
+import javafx.animation.FillTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerPilha implements Initializable {
+
+    private Linked_Stack<String> PE;
+
+    @FXML
+    private Button BotaoVoltar;
+
+    @FXML
+    private FlowPane FPDados;
+
+    @FXML
+    private TextField TopoConteudo;
+
+    @FXML
+    private TextField TFConsultaIndicePosicao;
+
+    @FXML
+    private TextField TFConsultaValorConteudo;
+
+    @FXML
+    private TextField TFInserirConteudo;
+
+    @FXML
+    private TextField TFInserirPosicao;
+
+    @FXML
+    private TextField TFNumeroDeElementos;
+
+    @FXML
+    private TextField TFRemoverPosicao;
+
+    @FXML
+    private AnchorPane paneConsultar1;
+
+    @FXML
+    private AnchorPane paneConsultar2;
+
+    @FXML
+    private AnchorPane paneInserir;
+
+    @FXML
+    private AnchorPane paneRemover;
+
+    @FXML
+    void EventoConsultaIndice(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EventoConsultaValor(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EventoInserir(MouseEvent event) {
+
+        String conteudo;
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle("ERRO");
+        conteudo = TFInserirConteudo.getText().trim();
+
+        if (conteudo.equalsIgnoreCase("")) {
+            alerta.setHeaderText("Sem Conteudo Valido");
+            alerta.setContentText("Por favor, preencha o campo de conteudo antes de apertar para inserir (apenas espaços nao sao validos)!");
+            alerta.showAndWait();
+            return;
+        }
+
+        this.PE.push(conteudo);
+        TFInserirConteudo.setText("");
+        TFNumeroDeElementos.setText(String.valueOf(PE.size()));
+        TopoConteudo.setText(PE.top());
+
+        // O desafio eh criar os blocos
+
+        FPDados.getChildren().add(0, bloco(conteudo));
+
+        this.animacao(0, 2, 1, "#8b0000", "#008B8B").play();
+
+
+    }
+
+    @FXML
+    void EventoRemover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EventoVoltar(MouseEvent event) throws IOException {
+        Stage stage = (Stage) BotaoVoltar.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("menuPrincipal.fxml"));
+        stage.setTitle("Estrutura de Dados");
+        stage.setScene(new Scene(root));
+    }
+
+    private FillTransition animacao(int contIndex, int cycles, double time, String fromColor, String toColor) {
+        VBox auxVBox = (VBox) FPDados.getChildren().get(contIndex);
+        StackPane auxSP = (StackPane) auxVBox.getChildren().get(0);
+        Rectangle auxRec = (Rectangle) auxSP.getChildren().get(0);
+
+        FillTransition transition = new FillTransition();
+        transition.setShape(auxRec);
+        transition.setFromValue(Color.web(fromColor));
+        transition.setToValue(Color.web(toColor));
+        transition.setCycleCount(cycles);
+        transition.setDuration(Duration.seconds(time));
+        return transition;
+    }
+
+
+
+    private VBox bloco(String conteudo) {
+        VBox novoNo = new VBox();
+        novoNo.setAlignment(Pos.CENTER);
+
+        StackPane sp = new StackPane();
+        sp.setPrefWidth(50);
+        sp.setPrefHeight(60);
+        sp.setAlignment(Pos.CENTER);
+
+        Rectangle retangulo = new Rectangle(50,50);
+        retangulo.getStyleClass().add("profile-boxes");
+        sp.getChildren().add(retangulo);
+
+        Text texto = new Text(conteudo);
+        texto.setFont(Font.font("Consolas", 15));
+        texto.setFill(Color.web("#ffffff"));
+        texto.setWrappingWidth(50);
+        texto.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
+        sp.getChildren().add(texto);
+
+        novoNo.getChildren().add(sp);
+
+        ImageView img = new ImageView("seta.png");
+        img.setFitWidth(30);
+        img.setFitHeight(30);
+        img.setRotate(90);
+        novoNo.getChildren().add(img);
+
+        return novoNo;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.PE = new Linked_Stack<>();
+        TFNumeroDeElementos.setText("0");
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(5.0); // Configurar o raio da sombra
+        dropShadow.setOffsetX(3.0); // Configurar o deslocamento horizontal da sombra
+        dropShadow.setOffsetY(3.0); // Configurar o deslocamento vertical da sombra
+        dropShadow.setColor(javafx.scene.paint.Color.BLACK); // Configurar a cor da sombra
+
+        paneConsultar1.setEffect(dropShadow);
+        paneInserir.setEffect(dropShadow);
+        paneConsultar2.setEffect(dropShadow);
+        paneRemover.setEffect(dropShadow);
+
+        Rectangle clip = new Rectangle();
+        clip.widthProperty().bind(FPDados.widthProperty());
+        clip.heightProperty().bind(FPDados.heightProperty());
+        FPDados.setClip(clip);
+        FPDados.setOrientation(Orientation.VERTICAL);
+        FPDados.setHgap(80);
+    }
+
+}
